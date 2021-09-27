0d4d51261bae02c153b419de48b872a0e578df19
Vaibhav Aher <vaibhavaher347@gmail.com>
2021-09-27 15:33:59 +0530

    ok

---
+++ WelcomeEmpWageSystem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3011313
+{
+    class WelcomeEmpWageSystem
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Calculation System");
+        }
+    }
+}
